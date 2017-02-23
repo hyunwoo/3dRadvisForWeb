b8c1b09fd58d49952d6bae71e76ebfb035981fc5
suhyun <suhyunlim2@gmail.com>
2017-02-23 21:29:50 +0900

    index page description input 작업 중

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,3 +1,28 @@
 /**
  * Created by suhyun on 2017. 2. 23..
  */
+
+function dataList(data){
+    var $list = $('.dataList .list');
+
+    var item = $('<div class="item"></div>');
+
+    _.map(data, function(d){
+        var target = item.clone().appendTo($list);
+        $(target).html(d)
+    });
+
+    var target = $('.dataList .list .item');
+
+    target.click(function(){
+        var $this = $(this);
+        if(!$this.hasClass('click')){
+            target.removeClass('click');
+            $this.addClass('click')
+        }
+    })
+}
+
+function controlDesc(){
+    var input = $('.viewer')
+}
