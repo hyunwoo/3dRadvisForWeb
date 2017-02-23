0904ddf55f63fb2f8efbe073df1d94056d6538ee
hyunwoo <h.hyunwoo@gmail.com>
2017-02-23 16:29:36 +0900

    Dialog, Toast 제작

--- src/uiStaticFunctions/uiStatics.js
+++ src/uiStaticFunctions/uiStatics.js
@@ -4,29 +4,84 @@
 const __UIStatic = new function () {
     var that = this;
 
-    this.$Toast = $('#toast');
+    var $Toast = $('#toast');
+    var toastTimeout;
     this.Toast = {
-        interval: undefined,
-        open: function (text) {
-            that.$Toast.addClass('open');
-            that.$Toast.find('.text').html(text);
+        removeTimeout: function () {
+            if (!_.isNil(toastTimeout)) clearTimeout(toastTimeout);
+        },
 
-            if (!_.isNil(this.interval)) clearTimeout(this.interval);
-            this.interval = setTimeout(this.close, 2500);
+        open: function (text) {
+            $Toast.addClass('open');
+            $Toast.find('.text').html(text);
+            that.Toast.removeTimeout();
+            toastTimeout = setTimeout(this.close, 2500);
         },
 
         close: function () {
-            that.$Toast.removeClass('open');
-            if (!_.isNil(this.interval)) clearTimeout(this.interval);
+            $Toast.removeClass('open');
+            that.Toast.removeTimeout();
+        },
+    };
+    $Toast.on('mouseover', this.Toast.removeTimeout);
+    $Toast.find('.closer').click(that.Toast.close);
+
+
+    var $Dialog = $('.dialogWrapper');
+    var $DialogTitle = $Dialog.find('.title');
+    var $DialogText = $Dialog.find('.text');
+    var $DialogPos = $Dialog.find('.pos');
+    var $DialogNeg = $Dialog.find('.neg');
+
+    this.Dialog = {
+        open: function (opt) {
+            $Dialog.addClass('open');
+            $DialogTitle.html(opt.title);
+            $DialogText.html(opt.text);
+
+            $DialogPos.html(opt.pos.name);
+            $DialogPos.unbind();
+            $DialogPos.click(function () {
+                that.Dialog.close();
+                opt.pos.action();
+            });
+
+            $DialogNeg.html(opt.neg.name);
+            $DialogNeg.unbind();
+            $DialogNeg.click(function () {
+                that.Dialog.close();
+                opt.neg.action();
+            });
+
+
+            // title
+            // text
+            // button
+            //  name
+            //  action
+        },
+
+        close: function () {
+            console.log('??');
+            $Dialog.removeClass('open');
         }
     };
-
-    this.Dialog = {};
-
     this.Message = {};
 };
 
 
-setTimeout(function () {
-    __UIStatic.Toast.open('asdfasdfasdfasdfasdf');
-}, 1000);
+//__UIStatic.Toast.open('asdfasdf');
+// __UIStatic.Dialog.open({
+//     title: "Lorem? Ipsum?",
+//     text: 'HANGLE AN DEN DA',
+//     pos: {
+//         name: 'Pos',
+//         action: function () {
+//         }
+//     },
+//     neg: {
+//         name: 'Neg',
+//         action: function () {
+//         },
+//     },
+// });
